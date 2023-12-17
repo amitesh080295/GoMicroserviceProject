f9e386119909c102e21bfbfabc15770886233b14
Amitesh Sinha <amitesh080295@gmail.com>
2023-12-16 23:12:58 -0500

    Update operations
    1. Enabled update operations for product, service and vendor

--- internal/database/database_product.go
+++ internal/database/database_product.go
@@ -7,6 +7,7 @@ import (
 	"github.com/amitesh080295/GoMicroserviceProject/internal/models"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func (c Client) GetAllProducts(ctx context.Context, vendorID string) ([]models.Product, error) {
@@ -41,4 +42,27 @@ func (c Client) GetProductById(ctx context.Context, ID string) (*models.Product,
 		return nil, result.Error
 	}
 	return product, nil
+}
+
+func (c Client) UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error) {
+	var products []models.Product
+	result := c.DB.WithContext(ctx).
+		Model(&products).
+		Clauses(clause.Returning{}).
+		Where(&models.Product{ProductID: product.ProductID}).
+		Updates(models.Product{
+			Name:     product.Name,
+			Price:    product.Price,
+			VendorID: product.VendorID,
+		})
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return nil, &dberrors.ConflictError{}
+		}
+		return nil, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return nil, &dberrors.NotFoundError{Entity: "product", ID: product.ProductID}
+	}
+	return &products[0], nil
 }
